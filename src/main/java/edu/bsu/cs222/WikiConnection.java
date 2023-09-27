2f6610abbb8c0cc293f8bc569bc1d804a414b5a5
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-09-26 20:23:33 -0400

    Create WikiConnection.java
    Basic implementation of WikiConnection class that connects to WIkipedia to fetch JSON data for a given article name.

---
+++ src/main/java/edu/bsu/cs222/WikiConnection.java
@@ -0,0 +1,66 @@
+package edu.bsu.cs222;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class WikiConnection
+{
+  private static final String WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php";
+  private static final String USER_AGENT = "WikipediaEditorTracker/1.0 (your-email@example.com)";
+
+  public String fetchArticleJsonData(String articleName)
+  {
+    String formattedArticleName = formatArticleNameForURL(articleName);
+    String apiUrl = buildApiUrl(formattedArticleName);
+
+    HttpURLConnection connection = null;
+    BufferedReader reader = null;
+    StringBuilder jsonContent = new StringBuilder;
+
+    try
+    {
+      URL url = new URL(apiUrl);
+      connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      connection.setRequestProperty("User-Agent", USER_AGENT);
+
+      InputStream inputStream = connection.getInputStream();
+      reader = new BufferedReader(new InputStreamReader(inputStream));
+
+      String line;
+      while ((line = reader.readLine()) != null)
+      {
+        jsonContent.append(line);
+      }
+    }
+      
+    finally
+    {
+      if (reader != null)
+      {
+        reader.close();
+      }
+      if (connection != null)
+      {
+        connection.disconnet();
+      }
+    }
+
+    return jsonContent.toString();
+  }
+
+  private String formatArticleNameForURL(String articleName)
+  {
+    return articleName.replace(" ", "_");
+  }
+
+  private String buildApiUrl(String formattedArticleName)
+  {
+    return String.format("%s?action=query&format=json&prop=revisions&titles=%s&rvprop=timestamp|user&rvlimit=13&redirects",
+            WIKIPEDIA_API_URL, formattedArticleName);
+  }
+}
