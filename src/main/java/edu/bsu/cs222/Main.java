5d772e6438e5ea8d18bbf3c34e58c311388e8dd9
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-09-26 20:09:34 -0400

    Update Main.java
    Added main functionality and structure of the program

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,7 +1,42 @@
 package edu.bsu.cs222;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        Scanner userInputScanner = new Scanner(System.in);
+        System.out.println("Welcome to the Wikipedia Editor Tracker!");
+        System.out.print("Enter the name of the Wikipedia article: ");
+        String articleName = userInputScanner.nextLine();
+
+        try
+        {
+            WikiConnection wikiConnection = new WikiConnection();
+            String articleJsonData = wikiConnection.fetchArticleJsonData(articleName);
+            JsonParser jsonParser = new JsonParser(articleJsonData);
+
+            System.out.println("Redirected to: " + jsonParser.getRedirectedArticleName());
+
+            ArrayList<Revision> recentRevisions = jsonParser.extractRecentRevisions();
+            System.out.println("Showing the last " + recentRevisions.size() + " edits:");
+            displayRecentRevisions(recentRevisions);
+        }
+
+        catch (IOException e)
+        {
+            System.err.println("Error: Unable to fetch data from Wikipedia.");
+        }
+    }
+
+    public static void displayRecentRevisions(ArrayList<Revision> revisionList)
+    {
+        for (Revision revision : revisionList)
+        {
+            System.out.println(revision.getTimestamp() + " " + revision.getUsername());
+        }
     }
 }
