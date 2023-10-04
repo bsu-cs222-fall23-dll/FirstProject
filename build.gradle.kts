0c37eaff89186bef7bc76a47b38215a98e372f24
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:04:47 -0400

    Update build.gradle.kts
    Added dependencies needed for program to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -12,8 +13,16 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    implementation("org.openjfx:javafx-controls:19")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass = "edu.bsu.cs222.GUI"
 }
