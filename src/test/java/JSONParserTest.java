30772bc3ff416a0ec02591549f50e51c7648ce8c
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:11:39 -0400

    Update JSONParserTest.java
    Changed functionality of JSONParserTest to better test the capabilities of the JSONParser class.

--- src/test/java/JSONParserTest.java
+++ src/test/java/JSONParserTest.java
@@ -1,63 +1,32 @@
-import org.junit.jupiter.api.BeforeEach;
+import edu.bsu.cs222.JSONParser;
+import edu.bsu.cs222.Revision;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class JSONParserTest {
-    private JSONParser jsonParser;
 
-    @BeforeEach
-    public void setUp() {
-        jsonParser = new JSONParser();
+    @Test
+    public void testGetRedirectsAsString() {
+        String jsonData = "{\"query\":{\"redirects\":[{\"to\":\"NewArticle\"}]}}";
+        JSONParser parser = new JSONParser(jsonData);
+
+        String expected = "Redirected to NewArticle\n";
+        String actual = parser.getRedirectsAsString();
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testParseValidJSONString() throws IOException {
-        // Mock an input stream with sample JSON data
-        String sampleJSON = "{\"revisions\":[{\"user\":\"User1\",\"timestamp\":\"2023-09-15T12:00:00Z\"}," +
-                "{\"user\":\"User2\",\"timestamp\":\"2023-09-14T14:30:00Z\"}]}";
-        InputStream inputStream = TestUtils.stringToInputStream(sampleJSON);
+    public void testConstructRevisionArrayList() {
+        String jsonData = "{\"query\":{\"pages\":{\"1\":{\"revisions\":[{\"timestamp\":\"2023-10-03T10:15:30Z\",\"user\":\"User1\"}]}}}}";
+        JSONParser parser = new JSONParser(jsonData);
 
-        // Parse the JSON data
-        jsonParser.parse(inputStream);
+        ArrayList<Revision> revisions = parser.constructRevisionArrayList();
 
-        // Validate the parsed data
-        assertEquals(2, jsonParser.getRevisions().size());
-        assertEquals("User1", jsonParser.getRevisions().get(0).getUser());
-        assertEquals("2023-09-15T12:00:00Z", jsonParser.getRevisions().get(0).getTimestamp());
-        assertEquals("User2", jsonParser.getRevisions().get(1).getUser());
-        assertEquals("2023-09-14T14:30:00Z", jsonParser.getRevisions().get(1).getTimestamp());
-    }
-
-    @Test
-    public void testParseEmptyJSONString() throws IOException {
-        // Mock an input stream with an empty JSON string
-        String emptyJSON = "{}";
-        InputStream inputStream = TestUtils.stringToInputStream(emptyJSON);
-
-        // Parse the JSON data
-        jsonParser.parse(inputStream);
-
-        // Validate that no revisions are parsed
-        assertEquals(0, jsonParser.getRevisions().size());
-    }
-
-    @Test
-    public void testParseInvalidJSONString() throws IOException {
-        // Mock an input stream with invalid JSON data
-        String invalidJSON = "Invalid JSON Data";
-        InputStream inputStream = TestUtils.stringToInputStream(invalidJSON);
-
-        // Parse the JSON data
-        jsonParser.parse(inputStream);
-
-        // Validate that parsing returns null revisions
-        assertNull(jsonParser.getRevisions());
+        assertEquals(1, revisions.size());
+        assertEquals("2023-10-03T10:15:30Z", revisions.get(0).getTimestamp());
+        assertEquals("User1", revisions.get(0).getUsername());
     }
 }
