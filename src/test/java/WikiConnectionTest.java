2e02240367468fa894128aca1acc3c73c87e8dbc
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:10:17 -0400

    Update WikiConnectionTest.java
    Changed functionality of WikiConnectionTest to better test for the capabilities of the WikiConnection class

--- src/test/java/WikiConnectionTest.java
+++ src/test/java/WikiConnectionTest.java
@@ -1,30 +1,56 @@
+import edu.bsu.cs222.WikiConnection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class WikiConnectionTest {
+
     private WikiConnection wikiConnection;
+    private HttpURLConnection mockConnection;
+    private BufferedReader mockReader;
+    private InputStream mockInputStream;
 
     @BeforeEach
     public void setUp() {
         wikiConnection = new WikiConnection();
+        mockConnection = mock(HttpURLConnection.class);
+        mockReader = mock(BufferedReader.class);
+        mockInputStream = mock(InputStream.class);
     }
 
     @Test
-    public void testGetJSONStringFromArticleName_ValidArticleName() throws IOException {
-        String articleName = "Java_programming_language";
-        String jsonString = wikiConnection.getJSONStringFromArticleName(articleName);
+    public void testFetchArticleJsonData() throws IOException {
+        String articleName = "Test_Article";
+        String expectedJsonData = "{\"query\":{\"pages\":{\"1\":{\"revisions\":[{\"timestamp\":\"2023-10-03T10:15:30Z\",\"user\":\"User1\"}]}}}}";
 
-        assertNotNull(jsonString);
-        assertFalse(jsonString.isEmpty());
+        when(mockConnection.getInputStream()).thenReturn(mockInputStream);
+        when(mockInputStream.read(any(byte[].class), anyInt(), anyInt())).thenReturn(-1);
+        when(mockReader.readLine()).thenReturn(expectedJsonData, null);
+
+        URL mockUrl = mock(URL.class);
+        when(mockUrl.openConnection()).thenReturn(mockConnection);
+
+        String jsonData = wikiConnection.fetchArticleJsonData(articleName);
+
+        assertEquals(expectedJsonData, jsonData);
+
+        verify(mockConnection).setRequestMethod("GET");
+        verify(mockConnection).setRequestProperty("User-Agent", "WikipediaEditorTracker/1.0 (your-email@example.com)");
+        verify(mockReader).close();
+        verify(mockConnection).disconnect();
     }
 
     @Test
-    public void testGetJSONStringFromArticleName_InvalidArticleName() {
-        String articleName = "InvalidArticleName";
-        assertThrows(IOException.class, () -> wikiConnection.getJSONStringFromArticleName(articleName));
+    public void testFormatArticleNameForURL() {
+        String articleName = "Test Article";
+        String expectedFormattedName = "Test_Article";
+
+        String formattedName = wikiConnection.formatArticleNameForURL(articleName);
+
+        assertEquals(expectedFormattedName, formattedName);
     }
 }
