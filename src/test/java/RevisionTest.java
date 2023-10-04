f4212ba0e1cdeb0501fe11b3dce018a05f4c71cc
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:09:08 -0400

    Update RevisionTest.java
    Changed RevisionTest functionality to better test the capabilities of the Revision class.

--- src/test/java/RevisionTest.java
+++ src/test/java/RevisionTest.java
@@ -1,25 +1,40 @@
-import org.junit.jupiter.api.BeforeEach;
+import edu.bsu.cs222.Revision;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RevisionTest {
-    private Revision revision;
 
-    @BeforeEach
-    public void setUp() {
-        revision = new Revision();
+    @Test
+    public void testGettersAndSetters() {
+        String timestamp = "2023-10-03T10:15:30Z";
+        String username = "User1";
+
+        Revision revision = new Revision(timestamp, username);
+
+        assertEquals(timestamp, revision.getTimestamp());
+        assertEquals(username, revision.getUsername());
+
+        String newTimestamp = "2023-10-04T09:30:45Z";
+        String newUsername = "User2";
+
+        revision.setTimestamp(newTimestamp);
+        revision.setUsername(newUsername);
+
+        assertEquals(newTimestamp, revision.getTimestamp());
+        assertEquals(newUsername, revision.getUsername());
     }
 
     @Test
-    public void testSetAndGetUser() {
-        revision.setUser("User1");
-        assertEquals("User1", revision.getUser());
-    }
+    public void testToString() {
+        String timestamp = "2023-10-03T10:15:30Z";
+        String username = "User1";
 
-    @Test
-    public void testSetAndGetTimestamp() {
-        revision.setTimestamp("2023-09-15T12:00:00Z");
-        assertEquals("2023-09-15T12:00:00Z", revision.getTimestamp());
+        Revision revision = new Revision(timestamp, username);
+
+        String expectedString = "2023-10-03T10:15:30Z User1";
+        String actualString = revision.toString();
+
+        assertEquals(expectedString, actualString);
     }
 }
