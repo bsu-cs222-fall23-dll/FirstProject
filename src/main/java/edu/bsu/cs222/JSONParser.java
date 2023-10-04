0a79c1fa94800ed8300a3e1c24c3416d732347ba
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:13:31 -0400

    Update JSONParser.java
    Changed functionality of JSONParser to better handle JSON data.

--- src/main/java/edu/bsu/cs222/JSONParser.java
+++ src/main/java/edu/bsu/cs222/JSONParser.java
@@ -1,10 +1,11 @@
-package edu.bsu.cs222
+package edu.bsu.cs222;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class JSONParser
 {
@@ -19,21 +20,20 @@ public class JSONParser
   {
     try
     {
-      JSONObject jsonObject = new JSONObject(jsonData);
-      JSONObject queryObject = jsonObject.getJSONObject("query");
-      JSONArray redirectsArray = queryObject.getJSONArray("redirects");
+      Map<String, Object> jsonObject = parseJSON(jsonData);
+      Map<String, Object> queryObject = (Map<String, Object>) jsonObject.get("query");
+      List<Object> redirectsArray = (List<Object>) queryObject.get("redirects");
 
-      if (redirectsArray.length() > 0)
+      if (!redirectsArray.isEmpty())
       {
-        JSONObject redirectObject = redirectsArray.getJSONObject(0);
-        String redirectTo = redirectObject.getString("to");
+        Map<String, Object> redirectObject = (Map<String, Object>) redirectsArray.get(0);
+        String redirectTo = (String) redirectObject.get("to");
         return "Redirected to " + redirectTo + "\n";
       }
     }
-
     catch (JSONException e)
     {
-      // Handle JSON parsing errors if necessary
+      handleJsonException(e);
     }
     return "";
   }
@@ -41,28 +41,57 @@ public class JSONParser
   public ArrayList<Revision> constructRevisionArrayList()
   {
     ArrayList<Revision> revisions = new ArrayList<>();
-  
+
     try
     {
-      JSONObject jsonObject = new JSONObject(jsonData);
-      JSONObject queryObject = jsonObject.getJSONObject("query");
-      JSONObject pagesObject = queryObject.getJSONObject("pages");
-      String pageId = pagesObject.keys().next(); //Get the first (and only) key
-  
-      JSONArray revisionsArray = pagesObject.getJSONObject(pageId).getJSONArray("revisions");
-      for (int i = 0; i < revisionsArray.length(); i++)
+      Map<String, Object> jsonObject = parseJSON(jsonData);
+      Map<String, Object> queryObject = (Map<String, Object>) jsonObject.get("query");
+      Map<String, Object> pagesObject = (Map<String, Object>) queryObject.get("pages");
+      Iterator<Map.Entry<String, Object>> pagesIterator = pagesObject.entrySet().iterator();
+      String pageId = pagesIterator.hasNext() ? pagesIterator.next().getKey() : null; // Get the first (and only) key
+
+      List<Object> revisionsArray = (List<Object>) ((Map<String, Object>) pagesObject.get(pageId)).get("revisions");
+      for (Object revisionObject : revisionsArray)
       {
-        JSONObject revisionObject = revisionsArray.getJSONObject(i);
-        String timestamp = revisionObject.getString("timestamp");
-        String username = revisionObject.getString("user");
+        Map<String, Object> revisionMap = (Map<String, Object>) revisionObject;
+        String timestamp = (String) revisionMap.get("timestamp");
+        String username = (String) revisionMap.get("user");
         revisions.add(new Revision(timestamp, username));
       }
     }
-  
     catch (JSONException e)
     {
-      // Handle JSON parsing errors if necessary
+      handleJsonException(e);
     }
+
+    return revisions;
+  }
+
+  // JSON parsing logic without external library
+  private Map<String, Object> parseJSON(String jsonData) throws JSONException
+  {
+    try
+    {
+      ObjectMapper objectMapper = new ObjectMapper();
+      return objectMapper.readValue(jsonData, Map.class);
+    }
+    catch (IOException e)
+    {
+      throw new JSONException("JSON parsing error: " + e.getMessage());
+    }
+  }
+
+  // Handle JSON parsing errors
+  private void handleJsonException(JSONException e) {
+    // Handle JSON parsing errors if necessary
+    System.err.println("JSON parsing error: " + e.getMessage());
+  }
+  private static class JSONException extends Exception {
+    public JSONException(String message) {
+      super(message);
+    }
+  }
+}
   
     return revisions;
   }
