be0cb6152b50f88b39df6c2b9511d9f53fabd70b
WarPotato16 <116615035+WarPotato16@users.noreply.github.com>
2023-10-03 23:14:34 -0400

    Update Main.java
    Made slight revisions to the functionality of the Main class.

--- src/main/java/edu/bsu/cs222/Main.java
+++ src/main/java/edu/bsu/cs222/Main.java
@@ -1,6 +1,5 @@
 package edu.bsu.cs222;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,27 +8,22 @@ public class Main
     public static void main(String[] args) 
     {
         Scanner userInputScanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Wikipedia Editor Tracker!");
         System.out.print("Enter the name of the Wikipedia article: ");
+
         String articleName = userInputScanner.nextLine();
+        WikiConnection wikiConnection = new WikiConnection();
+        String articleJsonData = wikiConnection.fetchArticleJsonData(articleName);
+        JSONParser jsonParser = new JSONParser(articleJsonData);
 
-        try
-        {
-            WikiConnection wikiConnection = new WikiConnection();
-            String articleJsonData = wikiConnection.fetchArticleJsonData(articleName);
-            JsonParser jsonParser = new JsonParser(articleJsonData);
+        System.out.println("Redirected to: " + jsonParser.getRedirectsAsString());
 
-            System.out.println("Redirected to: " + jsonParser.getRedirectedArticleName());
+        ArrayList<Revision> recentRevisions = jsonParser.constructRevisionArrayList();
 
-            ArrayList<Revision> recentRevisions = jsonParser.extractRecentRevisions();
-            System.out.println("Showing the last " + recentRevisions.size() + " edits:");
-            displayRecentRevisions(recentRevisions);
-        }
+        System.out.println("Showing the last " + recentRevisions.size() + " edits:");
 
-        catch (IOException e)
-        {
-            System.err.println("Error: Unable to fetch data from Wikipedia.");
-        }
+        displayRecentRevisions(recentRevisions);
     }
 
     public static void displayRecentRevisions(ArrayList<Revision> revisionList)
